3c87547a77a5a4bdb26b16f0509c1296a17c1ecd
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-09-03 19:49:16 +0300

    add mongoose

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import OpenAI  from "openai";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import mongoose from "mongoose";
 import readline from "readline";
+
 dotenv.config();
 
 const openai = new OpenAI({
@@ -12,30 +14,48 @@ const openai = new OpenAI({
 
 
 const app = express();
-const port = 8000;
+const PORT = process.env.PORT || 8000;
+
+mongoose.set('strictQuery', false);
+
+
+const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDb Connected: ${conn.connection.host} `);
+    }catch(error){
+        console.log(error);
+        process.exit(1);
+    }
+}
+
 app.use(bodyParser.json());
 app.use(cors());
 
 
 app.post('/chat-completion', async (req, res) => {
-    console.log(req.body)
+ 
     try {
       const response = await openai.chat.completions.create({
         model: "gpt-3.5-turbo",
         messages: [{ role: "user", content: req.body.message }],
       });
 
-     
+      const reply = response.choices[0].message.content;
   
-      const reply = response.data.choices[0].message.content;
+      console.log(reply);
+
       res.json({ reply });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
   });
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`);
 
-  });
+  connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}`)
+    })
+})
+
 
