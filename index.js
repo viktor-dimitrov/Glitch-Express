745ba2f1f0429e53dcb838384715b5ca59e1afcf
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-09-03 15:48:43 +0300

    1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import OpenAI  from "openai";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import readline from "readline";
 dotenv.config();
 
 const openai = new OpenAI({
@@ -16,7 +17,25 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
+app.post('/chat-completion', async (req, res) => {
+    console.log(req.body)
+    try {
+      const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: req.body.message }],
+      });
+
+     
+  
+      const reply = response.data.choices[0].message.content;
+      res.json({ reply });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
+
   });
 
